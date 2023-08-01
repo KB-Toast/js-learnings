a1036d8bcb433c03699c521ad60173baaef7ddd9
KB-Toast <kevin.brusson@gmail.com>
2023-08-01 15:26:01 +0200

    js basics

---
+++ control-03-monster-killer-starting-project/control-assignment-problem/assignment.js
@@ -0,0 +1,11 @@
+const randomNumber = Math.random(); // produces random number between 0 (including) and 1 (excluding)
+
+if (randomNumber > 0.7) {
+  alert('greater');
+}
+
+const arNumbers = [2, 7, 9, 7];
+
+for (let el in arNumbers) {
+  console.log(el);
+}
